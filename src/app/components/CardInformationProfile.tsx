7a539b9007e259702695f5f45ae1b43a74aeee01
ArmandoCode <totovip1464@gmail.com>
2023-06-27 22:45:12 -0600

    Creacion de componente para mostrar datos principales

---
+++ src/app/components/CardInformationProfile.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { formatDate } from "../utils/formatDate.";
+
+interface Props{
+    avatar_url: string,
+    name: string,
+    login: string,
+    bio: string,
+    created_at: string
+}
+
+export function CardInformationProfile({ avatar_url, name, login, bio, created_at }: Props) {
+
+    return(
+        <>
+            <section className="w-full h-auto grid grid-cols-[200px_minmax(300px,_1fr)] 
+             grid-rows-2 max-[650px]:grid-cols-[130px_minmax(150px,_1fr)]"
+            >
+                <Image 
+                    className="object-cover col-start-1 col-end-1 row-span-full rounded-full max-[650px]:w-[100px] max-[650px]:row-end-2"
+                    width={150} 
+                    height={100} 
+                    src={avatar_url} 
+                    alt="profile_avaar"
+                />
+
+                <div className="flex flex-col">
+                    <div className="flex flex-row items-center gap-24 max-[650px]:gap-0 max-[650px]:flex-col max-[650px]:items-start max-[650px]:">
+                        <div className="flex flex-col">
+                            <h2 className="text-2xl font-mono max-[650px]:text-xl">{ name }</h2>
+                            <p className="text-[#0175F7] font-light max-[650px]:text-sm" >{ `@${ login }` }</p>
+                        </div>
+                        <p className="font-mono text-slate-300 max-[650px]:text-sm">{ `Joinded ${ formatDate( created_at ) }` }</p>
+                    </div>
+                </div>
+
+                <div className="w-[400px] max-[650px]:w-[290px] max-[650px]:text-xs max-[650px]:mt-5">
+                    <p className="font-thin font-mono text-slate-400">{ bio }</p>
+                </div>
+
+            
+            </section>
+        </>
+    )
+
+}
