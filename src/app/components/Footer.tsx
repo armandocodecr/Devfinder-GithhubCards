3d3ca131c4a97b1f478850e667289769a0d56190
ArmandoCode <totovip1464@gmail.com>
2023-06-27 23:19:42 -0600

    Footer.tsx: Nuevo componente

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,23 @@
+
+
+export function Footer() {
+    return (
+        <footer className="absolute bottom-5 flex justify-center items-center max-[650px]:text-xs">
+            Made by &nbsp; 
+            <a 
+              className="font-bold cursor-pointer"
+              href="https://github.com/armandocodecr"
+              target="_blank"
+            >
+                ArmandoCodeCR
+            </a>  
+            &nbsp; from &nbsp; 
+            <img 
+              className="w-8"
+              src="https://em-content.zobj.net/thumbs/120/toss-face/342/flag-costa-rica_1f1e8-1f1f7.png" 
+              alt="img_costarica" 
+            />
+            &nbsp; with 💗
+        </footer>
+    )
+}
