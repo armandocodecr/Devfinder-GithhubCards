b64738eadcb4aa82a269799fd0263f4593eca6a1
ArmandoCode <totovip1464@gmail.com>
2023-06-27 22:46:32 -0600

    Creo archivo barril para exportar componentes

---
+++ src/app/components/index.ts
@@ -0,0 +1,5 @@
+export { AdditionalInformatioProfile } from "./AdditionalInformatioProfile";
+export { Stats } from "./Stats";
+export { CardInformationProfile } from "./CardInformationProfile";
+export { GithubCard } from "./GithubCard";
+export { Search } from "./Search";
