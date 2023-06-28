be8be8b58b5b86af32abb222aab84f0d19b2e5d7
ArmandoCode <totovip1464@gmail.com>
2023-06-27 23:19:54 -0600

    Agrego el footer a la pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { GithubCard, Search } from "./components/";
+import { Footer } from './components/Footer';
 
 export default function Home() {
   return (
     <main className="flex w-screen min-h-screen flex-col items-center justify-center">
       <Search />
       <GithubCard />
+      <Footer />
     </main>
   )
 }
