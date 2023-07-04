c60fb9ecc51bb88250b99983634b592a8457f47d
Erasmo Hernández <erasmoh@gmail.com>
2023-07-04 01:44:25 -0400

    Fix searchbar in wide size

--- src/app/components/GithubCard.tsx
+++ src/app/components/GithubCard.tsx
@@ -1,27 +1,34 @@
 "use client";
 import { CardInformationProfile, Stats, AdditionalInformatioProfile } from "./";
 import { useCard } from "../hooks/useCard";
-import { DownloadButton } from "./UI";
 
 export function GithubCard() {
-  
-  const { githubProfile, avatar_default, onMouseMoveEffect, 
-  onMouseOutEffect, ref, onDownloadCard } = useCard()
+  const {
+    githubProfile,
+    avatar_default,
+    onMouseMoveEffect,
+    onMouseOutEffect,
+    ref,
+    onDownloadCard,
+  } = useCard();
 
   return (
-    <section className='w-auto flex flex-col justify-center items-center'>
-      <div 
+    <>
+      <div
         className={`w-5/12 min-w-[800px] min-h-[400px] shadow h-auto bg-[#1F2A48] rounded-xl flex justify-center items-center mt-5 p-10 
-       ${githubProfile && !githubProfile.error ? ' transition-transform duration-75 hover:shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]' : ''}
+       ${
+         githubProfile && !githubProfile.error
+           ? " transition-transform duration-75 hover:shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]"
+           : ""
+       }
        max-[800px]:min-w-[650px] max-[650px]:min-w-[350px] max-[650px]:p-5 max-[400px]:min-w-[300px]`}
         id="github-card"
-        onMouseMove={e => onMouseMoveEffect(e)}
+        onMouseMove={(e) => onMouseMoveEffect(e)}
         onMouseOut={onMouseOutEffect}
         ref={ref}
       >
         <article className="w-full h-auto flex flex-col items-center">
           {githubProfile ? (
-      
             !githubProfile.error ? (
               <>
                 <CardInformationProfile
@@ -31,13 +38,13 @@ export function GithubCard() {
                   bio={githubProfile.bio}
                   created_at={githubProfile.created_at}
                 />
-      
+
                 <Stats
                   public_repos={githubProfile.public_repos}
                   followers={githubProfile.followers}
                   following={githubProfile.following}
                 />
-      
+
                 <AdditionalInformatioProfile
                   location={githubProfile.location}
                   blog={githubProfile.blog}
@@ -47,10 +54,9 @@ export function GithubCard() {
               </>
             ) : (
               <h1 className="text-4xl font-bold tracking-wide max-sm:text-xl">
-                  Username not found...
+                Username not found...
               </h1>
             )
-            
           ) : (
             <h1 className="text-4xl font-bold tracking-wide max-sm:text-xl">
               Search profile...
@@ -58,12 +64,16 @@ export function GithubCard() {
           )}
         </article>
       </div>
-
-     {
-      githubProfile && !githubProfile.error && (
-        <DownloadButton onDownloadCard={ onDownloadCard } />
-      )
-     }
-    </section>
+      {githubProfile && !githubProfile.error && (
+        <div
+          className="w-5/12 min-w-[800px] flex flex-end flex-col gap-3
+            max-[800px]:min-w-[650px] max-[650px]:min-w-[350px] max-[400px]:min-w-[300px]"
+        >
+          <button className="download-btn" onClick={onDownloadCard}>
+            Get your GithubCard
+          </button>
+        </div>
+      )}
+    </>
   );
 }
